911e97af0e3ca7c47fb453a6d30eda448a9aa848
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-09 06:09:44 +0600

    Create setup.shsetup.sh

---
+++ wsl/setup.shsetup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# May 2, 2023
+
+set -e
+
+cd `dirname ${BASH_SOURCE[0]}`/../vagrant
+export SHARED_DIR=`realpath shared`
+
+sudo -E bash -c "source debian/setup.sh; setup_packages"
+sudo -E bash -c "source debian/setup.sh; setup_cmocka_win"
+sudo -E bash -c "source debian/setup.sh; setup_wazuh_repo"
+sudo -E bash -c "source debian/setup.sh; setup_files"
+
+source debian/setup.sh
+setup_git
+setup_shell
+setup_ssh
